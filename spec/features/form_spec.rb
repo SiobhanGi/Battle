f3cf2dccf61d1e650c7c631692102a1add37ab37
Ellie Keen <elliekeen95@gmail.com>
2018-05-02 16:21:27 +0100

    Refactor with test helpers

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -1,9 +1,6 @@
 RSpec.feature "Form" do
   scenario "Players to fill in their name" do
-    visit "/" #should I change this to /form?
-    fill_in :player1, with: 'SmockinSam'
-    fill_in :player2, with: 'JerichoJosie'
-    click_button('Submit')
-    expect(page).to have_content("SmockinSam vs. JerichoJosie")
+    sign_in_and_play
+    expect(page).to have_content("Ellie vs. Siobhan")
   end
 end
--- spec/features/hp_spec.rb
+++ spec/features/hp_spec.rb
@@ -1,9 +1,6 @@
 RSpec.feature 'HP' do
   scenario 'view HP' do
-    visit '/'
-    fill_in :player1, with: 'Siobhan'
-    fill_in :player2, with: 'Ellie'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Siobhan: HP 100')
     expect(page).to have_content('Ellie: HP 100')
   end
