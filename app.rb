924647992edf0029cdd2e59eff7ea599417a61fd
Ellie Keen <elliekeen95@gmail.com>
2018-05-02 15:39:57 +0100

    Post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -4,15 +4,23 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   # set :sessions,  true
 
-   get '/' do
-     erb :form
-   end
+  enable :sessions
 
-   post '/names' do
-      @player1 = params[:player1]
-      @player2 = params[:player2]
-      erb :play
-   end
+  get '/' do
+    erb :form
+  end
+
+  post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect :play
+  end
+
+  get '/play' do
+    @player1  = session[:player1]
+    @player2 = session[:player2]
+    erb :play
+  end
 
 
 
--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-RSpec.feature "Home page", :type => :feature do
-  scenario "Homepage displays text" do
-    visit "/"
-    expect(page).to have_text("Testing infrastructure working!")
-  end
-end
