82a60c5129df91a3cf2d833e0d423b9382a8abcd
SiobhanGi <siobhan.goggins@gmail.com>
2018-05-03 10:05:49 +0100

    extracting logicattack broke

--- app.rb
+++ app.rb
@@ -10,26 +10,23 @@ class Battle < Sinatra::Base
      erb :form
    end
 
+   post '/names' do
+     $player1 = Player.new(params[:player1])
+     $player2 = Player.new(params[:player2])
+     redirect '/play'
+   end
+
    get '/play' do
      @player1 = $player1.name
      @player2 = $player2.name
      erb :play
    end
 
-  post '/names' do
-    $player1 = Player.new(params[:player1])
-    $player2 = Player.new(params[:player2])
-    redirect '/play'
-  end
-
   get '/attack' do
     @player1 = $player1.name
     @player2 = $player2.name
     erb :attack
   end
-
-
-
 
  # start the server if ruby file executed directly
  run! if app_file == $0
